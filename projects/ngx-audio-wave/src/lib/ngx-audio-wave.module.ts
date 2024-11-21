01da172f5729a6b37646a66827dc8e14a371cfd8
Nurlan Joldibaev <nurlan@payme.uz>
2024-11-21 15:24:16 +0500

    feat: v19.0.3, provideHttpClient with fetch

--- projects/ngx-audio-wave/src/lib/ngx-audio-wave.module.ts
+++ projects/ngx-audio-wave/src/lib/ngx-audio-wave.module.ts
@@ -1,15 +1,15 @@
 import {NgModule} from '@angular/core';
 import {NgxAudioWaveComponent} from './component/ngx-audio-wave.component';
-import {provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
+import {provideHttpClient, withFetch} from "@angular/common/http";
 import {CommonModule} from "@angular/common";
 import {NgxAudioWaveService} from "./service/ngx-audio-wave.service";
 
 
 @NgModule({
-    declarations: [NgxAudioWaveComponent],
-    exports: [NgxAudioWaveComponent],
-    imports: [CommonModule],
-    providers: [NgxAudioWaveService, provideHttpClient(withInterceptorsFromDi())]
+  declarations: [NgxAudioWaveComponent],
+  exports: [NgxAudioWaveComponent],
+  imports: [CommonModule],
+  providers: [NgxAudioWaveService, provideHttpClient(withFetch())]
 })
 export class NgxAudioWaveModule {
 }
